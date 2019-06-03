edf44da652a2ddbe6512677f0ed1fe920bd870a4
handomekal <handomekal@gmail.com>
2019-06-03 10:05:06 -0400

    html login page

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from "React"
+
+function Header() {
+    return (
+        <h1 style= {{color: "white", backgroundColor: "blue"}}>Bookware My Books, My Treasure</h1>
+        
+    )
+}
+
+export default Header
