4223d1df5a3623968a871472784d8b8cec10cdbc
bigalr8 <bigalr8@icloud.com>
2019-06-09 18:29:10 -0400

    filter saved books before showing API results

--- controllers/googleBooksController.js
+++ controllers/googleBooksController.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-//const db = require("../models");
+const db = require("../models");
 
 module.exports = {
   findAll: function(req, res) {
@@ -19,9 +19,18 @@ module.exports = {
           result =>
             result.volumeInfo.title  
         )
-        
       )
-       
+      //find all books already saved in the database	
+      //filter the results 
+      //checking every book in already saved in the database
+      //returning only results with ID not equal to that of a book already saved in the database    
+      .then(apiBooks =>
+        db.Book.find().then(dbBooks =>
+          apiBooks.filter(apiBook =>
+            dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
+          )
+        )
+      ) 
       .then(books => res.json(books))
       .catch(err => res.status(422).json(err));
   }
